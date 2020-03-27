adc03759473808c5258f2f1abb164eeb3e5431df
Glenn SHarman <glenn.r.sharman@gmail.com>
2020-03-26 21:35:37 -0500

    Minor update

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
-with open('/Users/gsharman/Box/detritalPy/pip_installation/README.md', "r") as fh:
+with open('README.md', "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="detritalpy",
     packages=['detritalpy'],    
-    version="1.3.9",
+    version="1.3.16",
     author="Glenn Sharman",
     author_email="gsharman@uark.edu",
     description="A Python-based toolset for visualizing and analyzing detrital geo-thermochronologic data",
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/grsharman/detritalpy",
-    download_url="https://github.com/grsharman/detritalPy/archive/v1.3.8.tar.gz",
+    download_url="https://github.com/grsharman/detritalPy/archive/v1.3.16.tar.gz",
     install_requires=['numpy','matplotlib','pandas','xlrd','folium','vincent','simplekml',
         'scipy','sklearn','statsmodels','peakutils'],
     classifiers=[
